905cf80fce99ef7196f762a4a432e6b76fabae97
pritatambe-code <pritamtambe7878@gmail.com>
2025-09-10 22:30:16 +0530

    Linked list reverse

--- src/High_level/Reverse_Linkedlist.java
+++ src/High_level/Reverse_Linkedlist.java
@@ -1,6 +1,9 @@
-package Linked_List;
+package High_level;
 
-public class Add_Last {
+// Online Java Compiler
+
+
+public class Reverse_Linkedlist {
     Node head;
     int size;
 
@@ -41,16 +44,31 @@ public class Add_Last {
         System.out.print("null");
     }
 
+    public void rev(){
+        Node currentNode = head;
+        Node previousNode = null;
+        Node next;
+
+        while(currentNode!= null){
+            next = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+        }
+        head = previousNode;
+    }
+
 
 
 
     public static void main(String[] args) {
-        Add_Last list = new Add_Last();
+        Reverse_Linkedlist list = new Reverse_Linkedlist();
         list.addLast("1");
         list.addLast("2");
         list.addLast("4");
         list.addLast("5");
         list.addLast("6");
+        list.printlist();
+        list.rev();
         list.printlist();
 
     }
